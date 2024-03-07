8d3365e19e66f5530bd6f0d08dc59845ca0940b3
Diego Rodriguez <poyutero@gmail.com>
2024-03-07 21:41:00 +0100

    Very basic expression implementation

--- implementExpression.m
+++ implementExpression.m
@@ -19,15 +19,24 @@ modelFileName = ['Model files' filesep 'polishedModel.mat'];
 modelFileName = [pwd filesep modelFileName];
 polishedModel = readCbModel(modelFileName);
 
+expA_data = readtable(['CSV' filesep 'exprA_ENSEMBL.csv']);
+
+expA_meta = readtable(['CSV' filesep 'exprA_meta.csv'], 'TextType','string');
+
 clear modelFileName
 %% Set boundaries
 % Set all bounds to default
 polishedModel = changeRxnBounds(polishedModel, polishedModel.rxns, 0, 'b');
 genelist = findGenesFromRxns(polishedModel, polishedModel.rxns);
 
+subgroup = strcmp(expA_meta{:,"Ectopic"}, 'TRUE');
+exprData.gene = expA_data(:,2:end).Properties.VariableNames';
+exprData.value = median(expA_data{subgroup,2:end})';
 % Calculate boundary values
-% exprbound = ;
-polishedModel = changeRxnBounds(polishedModel, polishedModel.rxns, exprbound, 'b');
+[expr, ~] = selectGeneFromGPR(polishedModel, exprData.gene, exprData.value, GPRparser(polishedModel));
+expr_bound = expr./median(expr)*1000;
+polishedModel = changeRxnBounds(polishedModel, polishedModel.rxns, expr_bound, 'u');
+
 
 %% Change objective function
 
